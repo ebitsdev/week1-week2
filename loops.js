1605b777d6477ba0caf3843b4931dfabca66360c
E. Bamba <ebamba@bitsnshine.com>
2019-02-27 00:00:47 -0500

    While loop

--- loops.js
+++ loops.js
@@ -20,3 +20,29 @@ const listOfStudents = [
 for(let i = 0; i < listOfStudents.length; i++){
     console.log(`Student number ${i+1}: ${listOfStudents[i]}`);
 }
+let counter = 0
+let correctAnswer
+// while(!listOfStudents){
+//     console.log(counter++)
+// }
+let count = 0
+while(count < 10){
+    console.log(`Counting from 0: ${count++}`)
+}
+let n = 0
+while(n < 3){
+   console.log(n++)
+}
+console.log(n)
+var i = 1
+let msg = ''
+do {
+    msg += i + ' x 5 = ' +  (i * 5)
+    i++
+} while( i < 1)
+// console.log(`The message: ${msg}`)
+while( i < 10){
+    msg += i + ' x 5 = ' + (i * 5)
+    i++
+}
+console.log(`The message: ${msg}`)
