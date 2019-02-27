51dcc9e2a2dd4a11a205ca1d7e0ed981a3292970
E. Bamba <ebamba@bitsnshine.com>
2019-02-26 19:45:12 -0500

    for loop

---
+++ loops.js
@@ -0,0 +1,22 @@
+const listOfStudents = [
+'Abebe',
+'Ayele',
+'shobeno',
+'Alamneh',
+'Tadesse',
+'Chereta',
+'Leonard',
+'Macairan',
+'Vaughan',
+'Abraha',
+'Mamo',
+'Tefera',
+'Lasic',
+'Kassa',
+'Wolde',
+'Kassa',
+'Peñaloza',
+]
+for(let i = 0; i < listOfStudents.length; i++){
+    console.log(`Student number ${i+1}: ${listOfStudents[i]}`);
+}
--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 // Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
 let question1 = prompt('Is my name Emmanuel?')
 // let question3 = prompt('Am I a teacher?')
-if (question1 === 'Emmanuel'){
+if (question1.toUpperCase() === 'Emmanuel'){
     confirm('Yes')
-    question1.toUpperCase()
+    question1
 } else {
     console.log('No')
 }
@@ -14,4 +14,18 @@ if (question2 === 'Hidden Figures'){
 }
 else {
     console.log('No')
+}
+var testing = 0
+testing = 1
+let parag = 'This is awesome'
+parag = 'This is so great'
+const name = 'Janae'
+name = 'Anna'
+const te4est = 'test'
+var el = document.getElementById('cost')
+el.textContent = 'Test dfdghfjdsfghjdsfhds'
+let myStudents = ['A', 'B', 'C']
+let car = {
+    color: 'blue',
+    model: 'RAV4'
 }
